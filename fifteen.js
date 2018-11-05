eeb26e5bc0a6105be13cbcda192c5854256a7b70
kyeekk <kaylaeffs1998@gmail.com>
2018-11-04 21:42:31 -0500

    attempt on sliding pieces

--- fifteen.js
+++ fifteen.js
@@ -11,39 +11,49 @@ window.onload = () =>{
         puzzlepiece.forEach((piece, i) => {
             // grid layout
             piece.classList.add("puzzlepiece");
-            piece.classList.add("movablepiece");
+            //piece.classList.add("movablepiece");
             piece.style.left = `${(i % x) * 100}px`;
             piece.style.top = `${parseInt(i / x) * 100}px`;
             bgPos = `-${piece.style.left} -${piece.style.top}`;
             piece.style.backgroundPosition = bgPos;
         });
 
+        piece.addEventListener("mouseover", canMove)
         piece.addEventListener("click", movePiece);
     }
+
+    function canMove(){
+        if ( moveHorizontal(this) || moveVertical(this)){
+            this.classList.add("movablepiece");
+        }
+    }
+
     function movePiece(){
-        if (moveHorizontal){
+        if (moveHorizontal(this)){
             place = parseInt(this.style.left);
             this.style.left = `${blank[0]}px`;
             blank[0] = place;
         }
-        if (moveVertical){
+        if (moveVertical(this)){
             place = parseInt(this.style.top);
-            this.style.top = `${blankPiece[1]}px`;
+            this.style.top = `${blank[1]}px`;
             blank[1] = place;
         }
     }
 
-    function moveHorizontal(){
-        if (parseInt(this.style.top) == blank[1]){
-            if(parseInt(this.style.left) - blank[0] == 100 || parseInt(this.style.left) - blank[0] == -100){
+    function moveHorizontal(piece){
+        if (parseInt(piece.style.top) == blank[1]){
+            if(parseInt(piece.style.left) - blank[0] == 100 || 
+            parseInt(piece.style.left) - blank[0] == -100){
                 return true
             }
         }
     }
 
-    function moveVertical(){
-        if (parseInt(this.style.left) == blank[0]){
-            if(parseInt(this.style.top) - blank[0] == 100 || parseInt(this.style.top) - blank[0] == -100){
+    function moveVertical(piece){
+        if (parseInt(piece.style.left) == blank[0]){
+            if(parseInt(piece.style.top) - blank[0] == 100 || 
+            parseInt(piece.style.top) - blank[0] == -100){
                 return true
             }
         }
